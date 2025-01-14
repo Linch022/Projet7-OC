c47c3fe1c77f404fba81db7980dfd34b3daa1f50
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2025-01-14 11:44:51 +0100

    installation scss

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter as Router, Routes, Route} from "react-router"
-import './index.css'
-import App from './App.jsx'
 import Error from './pages/Error.jsx'
+import Home from './pages/Home';
+import HousingSheet from './pages/HousingSheet';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path='/' element={<App />} />
+        <Route path='/' element={<Home />} />
         <Route path='*' element={<Error />}/>
+        <Route path='/logement' element={<HousingSheet />}/>
       </Routes>
     </Router>
   </StrictMode>,
