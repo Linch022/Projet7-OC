ae2cb4f3448a6d1f3aafe9928a374ca5d96d24f6
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2025-01-14 11:53:33 +0100

    ajout de la page à propos et de sa route

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -4,14 +4,16 @@ import {BrowserRouter as Router, Routes, Route} from "react-router"
 import Error from './pages/Error.jsx'
 import Home from './pages/Home';
 import HousingSheet from './pages/HousingSheet';
+import About from './pages/About.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='*' element={<Error />}/>
         <Route path='/logement' element={<HousingSheet />}/>
+        <Route path='/a-propos' element={<About />} />
+        <Route path='*' element={<Error />}/>
       </Routes>
     </Router>
   </StrictMode>,
