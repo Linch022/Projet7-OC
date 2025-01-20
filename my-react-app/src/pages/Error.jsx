5474bfdac5b41aa26f84e2e165a57999bdf2f5cf
Berthollet Nicolas <berthollet.nicolas@gmail.com>
2025-01-20 16:04:50 +0100

    Page error finalisée avec responsive

--- my-react-app/src/pages/Error.jsx
+++ my-react-app/src/pages/Error.jsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router';
+import Home from './Home';
+
 function Error() {
   return (
-    <div>Error</div>
-  )
+    <div className='error-page'>
+      <p className='error-page__nmb'>404</p>
+      <p className='error-page__txt'>
+        Oups! La page que vous demandez n&apos;existe pas.
+      </p>
+      <Link to='/' className='error-page__link'>
+        Retourner sur la page d&apos;accueil
+      </Link>
+    </div>
+  );
 }
 
-export default Error
+export default Error;
