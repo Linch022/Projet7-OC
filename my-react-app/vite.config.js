d59347070a7ff8d143f2c475970d2dbd32bd566f
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2025-06-18 11:10:22 +0200

    initialisation du deploiement

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-  },
-  build: {
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/styles.css',
-      },
-    },
-  },
+	plugins: [react()],
+	base: "/Projet7-OC/",
+	server: {
+		host: true,
+		port: 3000,
+	},
+	build: {
+		cssCodeSplit: true,
+		rollupOptions: {
+			output: {
+				assetFileNames: "assets/styles.css",
+			},
+		},
+	},
 });
